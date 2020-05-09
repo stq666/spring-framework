75f5ff6248dd63356ecf27d94cb2698e4b771f04
stqsht <stqsht@163.com>
2020-05-09 21:37:49 +0800

    成功构建项目

---
+++ stq-test/src/main/java/com/stq/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.stq.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author stqsht@163.com
+ * @date 2020-05-09 21:12
+ */
+@Configuration
+public class AppConfig {
+}
---
+++ stq-test/src/test/java/com/stq/test/AppConfigTest.java
@@ -0,0 +1,17 @@
+package com.stq.test;
+
+import com.stq.config.AppConfig;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author stqsht@163.com
+ * @date 2020-05-09 21:13
+ */
+public class AppConfigTest {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		AppConfig bean = applicationContext.getBean(AppConfig.class);
+		System.out.println(bean);
+	}
+}
